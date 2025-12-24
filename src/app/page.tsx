2bb689ad1af515426a803f11eec19b11f31fe0a8
Dipak Gohil <dipak@simformsolutions.com>
2025-12-24 10:31:26 +0530

    favicon updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-secondary-foreground p-8">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-8">
       <main className="flex flex-col items-center gap-8 text-center">
-        <h1 className="text-4xl font-bold text-white">
+        <Image
+          src="/images/next.png"
+          alt="Next.js + Shadcn Logo"
+          width={150}
+          height={150}
+        />
+        <h1 className="text-4xl font-bold text-black">
           Next.js + Shadcn Boilerplate
         </h1>
-        <p className="max-w-2xl text-white/90">
+        <p className="max-w-2xl text-black/90">
           A modern Next.js boilerplate with shadcn/ui components, TypeScript,
           and Tailwind CSS.
         </p>
